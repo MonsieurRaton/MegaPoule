91833b12c1d7c4cd63d0ff49de124c185f33a057
PitouGames <pitou.games@gmail.com>
2017-05-03 17:25:58 +0200

    Ajout du projet Unity

---
+++ MegaPouleUnity/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] private int height;
+    [SerializeField] private int distance;
+    [SerializeField] private Transform target;
+    [SerializeField] private float smooth;
+
+    /*void Start () {
+		
+	}*/
+	
+	void Update () {
+        Vector3 targetPosition = target.localPosition + (Vector3.forward * distance) + (Vector3.up * height);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, smooth*Time.deltaTime);
+
+    }
+}
---
+++ MegaPouleUnity/Assets/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody myRigidbody;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private float translationSpeed;
+
+    void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && Physics.Raycast(transform.localPosition + Vector3.up, Vector3.down, 1.2f)) {
+            myRigidbody.AddForce(Vector3.up*jumpPower);
+        }
+
+        Debug.DrawLine(transform.localPosition + Vector3.up, transform.localPosition+(Vector3.down*0.2f), Color.green);
+
+        float horizontal = Input.GetAxisRaw("Horizontal"); // 0 1 ou -1
+        myRigidbody.AddForce((horizontal * translationSpeed) * Vector3.right, ForceMode.VelocityChange);
+        if (horizontal != 0f) {
+            //
+            //myRigidbody.MovePosition(transform.localPosition + Vector3.right * horizontal * translationSpeed * Time.deltaTime);
+            if (horizontal > 0) {
+                myRigidbody.MoveRotation(Quaternion.Euler(0, -90, 0));
+            } else {
+                myRigidbody.MoveRotation(Quaternion.Euler(0, 90, 0));
+            }
+        }
+
+        if (transform.localPosition.y < -10) {
+            transform.localPosition = new Vector3(3, 2, 0);
+            transform.localEulerAngles = new Vector3(0, 0, 0);
+            myRigidbody.velocity = Vector3.zero;
+        }
+
+    }
+
+
+
+}
