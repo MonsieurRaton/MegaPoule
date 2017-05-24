95e9c58771691b543c6fc0b65eb1a378bc63c1c0
PitouGames <pitou.games@gmail.com>
2017-05-24 22:46:30 +0200

    Début gestion de "Kopter"
    Ajout d'un script et mise en place de l'animator.

---
+++ MegaPouleUnity/Assets/Scripts/Kopter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kopter : MonoBehaviour {
+
+    [SerializeField] private float rotorRotationSpeed;
+    [SerializeField] private Transform rotorBone;
+    private Animator myAnimator;
+
+    void Start () {
+        myAnimator = GetComponent<Animator>();
+    }
+	
+	void Update () {
+        rotorBone.Rotate(0, 0, rotorRotationSpeed * Time.deltaTime, Space.Self);
+    }
+
+
+    private void OnCollisionEnter(Collision collision) {
+            myAnimator.Play("Hit");
+            if (collision.transform.CompareTag("Projectile")) {
+                Destroy(collision.gameObject);
+            }
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        print(other.tag);
+        if (other.CompareTag("Player")) {
+            rotorRotationSpeed *= 2;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) {
+            rotorRotationSpeed /= 2;
+        }
+    }
+
+
+}
