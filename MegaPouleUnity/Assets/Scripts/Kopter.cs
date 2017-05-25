4e50da9ab334c8407c5df24573cf6f0ef6fb6281
PitouGames <pitou.games@gmail.com>
2017-05-25 21:11:45 +0200

    Patiplap et Kopter
    - Ajout du clignottement de Kopter quand est heurté
    - Création de l'animator pour Patiplat et début des interractions

--- MegaPouleUnity/Assets/Scripts/Kopter.cs
+++ MegaPouleUnity/Assets/Scripts/Kopter.cs
@@ -6,10 +6,14 @@ public class Kopter : MonoBehaviour {
 
     [SerializeField] private float rotorRotationSpeed;
     [SerializeField] private Transform rotorBone;
+    [SerializeField] private Color hitColor;
     private Animator myAnimator;
+    private Material myMaterial;
+    private IEnumerator coBlinking;
 
     void Start () {
         myAnimator = GetComponent<Animator>();
+        myMaterial = GetComponentInChildren<SkinnedMeshRenderer>().material;
     }
 	
 	void Update () {
@@ -18,15 +22,14 @@ public class Kopter : MonoBehaviour {
 
 
     private void OnCollisionEnter(Collision collision) {
-            myAnimator.Play("Hit");
-            if (collision.transform.CompareTag("Projectile")) {
-                Destroy(collision.gameObject);
-            }
-        
+        myAnimator.Play("Hit");
+        if (collision.transform.CompareTag("Projectile")) {
+            Destroy(collision.gameObject);
+        }
+        Blink();
     }
 
     private void OnTriggerEnter(Collider other) {
-        print(other.tag);
         if (other.CompareTag("Player")) {
             rotorRotationSpeed *= 2;
         }
@@ -38,5 +41,22 @@ public class Kopter : MonoBehaviour {
         }
     }
 
+    public void Blink() {
+        if (coBlinking != null) {
+            StopCoroutine(coBlinking);
+        }
+        coBlinking = Blinking();
+        StartCoroutine(coBlinking);
+    }
+
+    private IEnumerator Blinking() {
+        WaitForSeconds wait = new WaitForSeconds(0.08f);
+        for (int i = 0; i<3; i++) {
+            myMaterial.color = hitColor;
+            yield return wait;
+            myMaterial.color = Color.white;
+            yield return wait;
+        }
+    }
 
 }
---
+++ MegaPouleUnity/Assets/Scripts/Platiplap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platiplap : MonoBehaviour {
+    
+    private Animator myAnimator;
+
+    void Start() {
+        myAnimator = GetComponent<Animator>();
+    }
+
+	// Update is called once per frame
+	/*void Update () {
+		
+	}*/
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            myAnimator.SetBool("Active", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) {
+            myAnimator.SetBool("Active", false);
+        }
+    }
+}
