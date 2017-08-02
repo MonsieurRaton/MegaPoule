10cf1dbeb290296301341c4f95f312a01409a7d5
PitouGames <pitou.games@gmail.com>
2017-08-02 13:11:10 +0200

    Petite maj
    - Passage Unity 5.6.2f1
    - Modifications imports de certains assets
    - Animations de la poule et changemnet du comportement

--- MegaPouleUnity/Assets/Scripts/Player.cs
+++ MegaPouleUnity/Assets/Scripts/Player.cs
@@ -5,32 +5,43 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     private Rigidbody myRigidbody;
+    private Animator myAnimator;
     [SerializeField] private float jumpPower;
     [SerializeField] private float translationSpeed;
     [SerializeField] private GameObject projectile;
+    /// <summary> Indique si la poule touche le sol. </summary>
+    [SerializeField] private bool isGrounded;
 
     void Start () {
         myRigidbody = GetComponent<Rigidbody>();
+        myAnimator = GetComponentInChildren<Animator>();
     }
 	
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.UpArrow) && Physics.Raycast(transform.localPosition + Vector3.up, Vector3.down, 1.2f)) {
+
+        isGrounded = Physics.Raycast(transform.localPosition + Vector3.up, Vector3.down, 1.2f);
+        myAnimator.SetBool("IsJumping", !isGrounded); // bool "IsJumping"
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded) {
             myRigidbody.AddForce(Vector3.up*jumpPower);
         }
+
 #if UNITY_EDITOR
         Debug.DrawLine(transform.localPosition + Vector3.up, transform.localPosition+(Vector3.down*0.2f), Color.green);
 #endif
         float horizontal = Input.GetAxisRaw("Horizontal"); // 0 1 ou -1
-        myRigidbody.AddForce((horizontal * translationSpeed) * Vector3.right, ForceMode.VelocityChange);
+
         if (horizontal != 0f) {
-            //
-            //myRigidbody.MovePosition(transform.localPosition + Vector3.right * horizontal * translationSpeed * Time.deltaTime);
+
+            //myRigidbody.AddForce((horizontal * translationSpeed) * Vector3.right, ForceMode.VelocityChange);
+            
+            myRigidbody.MovePosition(transform.localPosition + Vector3.right * (horizontal * translationSpeed * Time.deltaTime));
             if (horizontal > 0) {
                 myRigidbody.MoveRotation(Quaternion.Euler(0, 90, 0));
             } else {
                 myRigidbody.MoveRotation(Quaternion.Euler(0, -90, 0));
             }
         }
+        myAnimator.SetBool("Run", horizontal != 0f);
 
         if (transform.localPosition.y < -10) { // Respawn
             transform.localPosition = new Vector3(3, 2, 0);
--- MegaPouleUnity/Assets/Scripts/Projectile.cs
+++ MegaPouleUnity/Assets/Scripts/Projectile.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
 
     [SerializeField] private float startSpeed;
+    [SerializeField] private Mesh[] meshs;
 
-	void Start () {
+    void Start() {
         GetComponent<Rigidbody>().velocity = transform.forward * startSpeed; // Mise en place vitesse initiale.
+        GetComponent<MeshFilter>().mesh = meshs[Random.Range(0, meshs.Length)];
         Destroy(gameObject, 5f); // Suppression au bout de 5 secondes.
-	}
-	
+    }
 
 }
