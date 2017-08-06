8263fbbcfa868e65a2a29757d920662b0d87ac8c
PitouGames <pitou.games@gmail.com>
2017-08-06 12:06:05 +0200

    Corrections mineures
    + ajoute d'un script basique pour la destruction des caisses

---
+++ MegaPouleUnity/Assets/Caisse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caisse : MonoBehaviour {
+
+    [SerializeField] private int life;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.transform.CompareTag("Projectile")) {
+            life--;
+            if (life <= 0) {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
